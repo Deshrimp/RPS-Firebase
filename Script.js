80280d64255a9a41f8c11e0fcd474e7f241783d0
Jessica Y <desh365@hotmail.com>
2018-09-23 02:10:31 -0500

    saves user choice

--- Script.js
+++ Script.js
@@ -8,16 +8,76 @@ var config = {
   messagingSenderId: "761687311611"
 }
 firebase.initializeApp(config)
+// Create a variable to reference the database.
+var database = firebase.database()
+var con
+var userName = ""
+var userid = ""
+var userSelection = ""
 
-//User will only be able to pick one option
-/*function selectOnlyThis(id) {
-for (var i = 1; i <= 3; i++) {
-    document.getElementById(i).checked = false
+// All of our connections will be stored in this directory.
+var connectionsRef = database.ref("/connections")
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected")
+
+// When the client's connection state changes...
+connectedRef.on("value", async function (snap) {
+  // If they are connected..
+  if (snap.val()) {
+    // Add user to the connections list.
+    con = await connectionsRef.push(true)
+    userid = con.key
+    // Remove user from tshe connection list when they disconnect.
+    con.onDisconnect().remove()
   }
-  document.getElementById(id).checked = true
-}
-*/
-$(".checkbox").on("click", function() {
-  $("#checkboxes").text("")
-  var userSelection = this.id
 })
+
+// When first loaded or when the connectons list changes...
+connectionsRef.on("value", function (snap) {
+  if (snap.numChildren() <= 1) {
+    userName = "Ragnar"
+  }
+  else {
+    userName = "Rollo"
+  }
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#watchers").text(snap.numChildren())
+
+})
+
+
+//About Once: Listens for exactly one event of the specified event type, and then stops listening.
+//This is equivalent to calling on(), and then calling off() inside the callback function.
+connectionsRef.once("value", snap => {
+  if (snap.numChildren() > 2)
+    window.location.replace("./sorry.html")
+})
+database.ref().on("value", function (snapshot) {
+  $(".checkbox").on("click", function () {
+    $("#checkboxes").text("")
+    userSelection = this.id
+    var userKey = database.ref().push({
+      name: userName,
+      user: userid,
+      selection: userSelection,
+    }).getKey()
+
+    var useer = snapshot.val().userKey
+    console.log(useer)
+    /*if (userName === "Ragnar") {
+      var ragnarSelection = userSelection
+      console.log("I'm Ragnar")
+      console.log(ragnarSelection)
+    }
+    else {
+      var rolloSelection = snapshot.child("Rollo".selection)
+      console.log("I'm Rollo")
+      console.log(rolloSelection)
+    }*/
+  })
+})
+
