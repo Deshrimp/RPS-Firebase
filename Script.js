99fd8e6f86a7b40a9664827db716c2f7632df0ad
Jessica Y <desh365@hotmail.com>
2018-09-24 17:05:08 -0500

    fixed bug

--- Script.js
+++ Script.js
@@ -14,17 +14,24 @@ var con
 var userName = ""
 var userid = ""
 var userSelection = ""
+var printname
+var printid
+var printselection
+var Ragnarpick
+var Rollopick
+var Ragnarwins = 0
+var Rollowins = 0
+var draw = 0
+var movesRef = database.ref("moves")
 
 // All of our connections will be stored in this directory.
 var connectionsRef = database.ref("/connections")
-
 // '.info/connected' is a special location provided by Firebase that is updated every time
 // the client's connection state changes.
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
 var connectedRef = database.ref(".info/connected")
-
 // When the client's connection state changes...
-connectedRef.on("value", async function (snap) {
+connectedRef.on("value", async function(snap) {
   // If they are connected..
   if (snap.val()) {
     // Add user to the connections list.
@@ -36,103 +43,84 @@ connectedRef.on("value", async function (snap) {
 })
 
 // When first loaded or when the connectons list changes...
-connectionsRef.once("value", function (snap) {
+//We name the player when they get in
+connectionsRef.once("value", function(snap) {
   if (snap.numChildren() <= 1) {
     userName = "Ragnar"
     $("#userid").text("You are Ragnar")
-  }
-  else {
+  } else {
     userName = "Rollo"
     $("#userid").text("You are Rollo")
   }
   // Display the viewer count in the html.
   // The number of online users is the number of children in the connections list.
   $("#watchers").text(snap.numChildren())
-
 })
 
-var postsRef = database.ref().child("posts")
-
-//About Once: Listens for exactly one event of the specified event type, and then stops listening.
-//This is equivalent to calling on(), and then calling off() inside the callback function.
+//Eliminate a third connection
 connectionsRef.once("value", snap => {
-  if (snap.numChildren() > 2)
-    window.location.replace("./sorry.html")
+  if (snap.numChildren() > 2) window.location.replace("./sorry.html")
 })
-var printname
-var printid
-var printselection
-var Ragnarpick
-var Rollopick
-var Ragnarwins = 0
-var Rollowins = 0
-var draw = 0
-var leadsRef = database.ref('posts');
-$(".checkbox").on("click", function () {
+
+movesRef.on("child_added", function(snapshot) {
+  const { name, selection } = snapshot.val()
+  if (name === "Ragnar") {
+    Ragnarpick = selection
+  } else if (name === "Rollo") {
+    Rollopick = selection
+  }
+  if (name === userName) {
+    $("#myChoice").append(selection)
+  }
+
+  if (Ragnarpick && Rollopick) {
+    $("#RagnarPick").append(Ragnarpick)
+    $("#RolloPick").append(Rollopick)
+    if (Ragnarpick === "Rock" && Rollopick === "Paper") {
+      Rollowins++
+      $("#whowins").text("Rollo Wins: " + Rollowins)
+    } else if (Ragnarpick === "Rock" && Rollopick === "Scissors") {
+      Ragnarwins++
+      $("#whowins").text("Ragnar Wins: " + Ragnarwins)
+    } else if (Ragnarpick === "Scissors" && Rollopick === "Rock") {
+      $("#whowins").text("Rollo Wins: " + Rollowins)
+    } else if (Ragnarpick === "Scissors" && Rollopick === "Paper") {
+      $("#whowins").text("Ragnar Wins: " + Ragnarwins)
+    } else if (Ragnarpick === "Paper" && Rollopick === "Rock") {
+      Ragnarwins++
+      $("#whowins").text("Ragnar Wins: " + Ragnarwins)
+    } else if (Ragnarpick === "Paper" && Rollopick === "Scissors") {
+      Rollowins++
+      $("#whowins").text("Rollo Wins: " + Rollowins)
+    } else if (Ragnarpick === Rollopick) {
+      draw++
+      $("#whowins").text("It was a draw: " + draw)
+    }
+    nextMatch()
+  }
+})
+
+$(".checkbox").on("click", function() {
   $("#checkboxes").text("")
   userSelection = this.id
-  var newPostRef = postsRef.push()
-  var postId = newPostRef.key
-  newPostRef.set({
+  movesRef.push({
     name: userName,
     user: userid,
-    selection: userSelection,
-    idkey: postId
+    selection: userSelection
   })
-  leadsRef.on('value', function (snapshot) {
+})
+
+/*
+  movesRef.on('value', function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
       printname = childSnapshot.val().name;
       printid = childSnapshot.val().user;
       printselection = childSnapshot.val().selection;
     });
-    leadsRef.once('child_added', function (snapshot) {
-      if (printname === "Ragnar") {
-        Ragnarpick = printselection
-        $("#RagnarPick").append(Ragnarpick)
-      } else if (printname === "Rollo") {
-        Rollopick = printselection
-        $("#RolloPick").append(Rollopick)
-      }
-      if (Ragnarpick === "Rock" && Rollopick === "Paper") {
-        Rollowins++
-        console.log("rollowins" + Rollowins)
-        $("#whowins").text("Rollo Wins: " + Rollowins)
-      }
-      else if (Ragnarpick === "Rock" && Rollopick === "Scissors") {
-        Ragnarwins++
-        console.log("ragnarwins" + Ragnarwins)
-        $("#whowins").text("Ragnar Wins: " + Ragnarwins)
-      }
-      else if (Ragnarpick === "Scissors" && Rollopick === "Rock") {
-        Rollowins++
-        console.log("rollowins" + Rollowins)
-        $("#whowins").text("Rollo Wins: " + Rollowins)
-      }
-      else if (Ragnarpick === "Scissors" && Rollopick === "Paper") {
-        Ragnarwins++
-        console.log("ragnarwins" + Ragnarwins)
-        $("#whowins").text("Ragnar Wins: " + Ragnarwins)
-      }
-
-      else if (Ragnarpick === "Paper" && Rollopick === "Rock") {
-        Ragnarwins++
-        console.log("ragnarwins" + Ragnarwins)
-        $("#whowins").text("Ragnar Wins: " + Ragnarwins)
-      }
-
-      else if (Ragnarpick === "Paper" && Rollopick === "Scissors") {
-        Rollowins++
-        console.log("rollowins" + Rollowins)
-        $("#whowins").text("Rollo Wins: " + Rollowins)
-      }
-      else if (Ragnarpick === Rollopick) {
-        draw++
-        $("#whowins").text("It was a draw: " + draw)
-      }
-    });
-  });
 
 
-})
-
-
+})*/
+function nextMatch() {
+  console.log("this goes to next question")
+  movesRef.remove()
+}
